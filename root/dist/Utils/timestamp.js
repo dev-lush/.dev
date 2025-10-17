41422b2e77a0ce2071baa864cda33ab3aa8a95fc
LushDashBlaze <lushdashblaze@gmail.com>
2025-10-17 10:14:45 +0700

    Delete root/dist directory

--- root/dist/Utils/timestamp.js
+++
@@ -1,8 +0,0 @@
-export function formatDiscordTimestamps(date) {
-    const timestampSeconds = Math.floor(date.getTime() / 1000);
-    return {
-        relative: `<t:${timestampSeconds}:R>`,
-        longDate: `<t:${timestampSeconds}:D>`,
-        shortTime: `<t:${timestampSeconds}:t>`
-    };
-}
